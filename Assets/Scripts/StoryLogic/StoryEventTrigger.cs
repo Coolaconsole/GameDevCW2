af2aa1398f857930462940ed6b0e25ba769b46db
Ethan <ethan@shillitos.org.uk>
2025-12-16 09:59:32 +0000

    Updated story manager with dict to store story flags, with the ablity for other classes to get the state of a given event and to bind to a delegate for when the value of an event changes

--- Assets/Scripts/StoryLogic/StoryEventTrigger.cs
+++ Assets/Scripts/StoryLogic/StoryEventTrigger.cs
@@ -3,16 +3,19 @@ using UnityEngine;
 public class StoryEventTrigger : MonoBehaviour
 {
     public StoryEventInfo info;
-    public StoryManager manager;
+    StoryManager manager;
 
     private void Start()
     {
+        //set manager - done this way as won't be assigned in scene as will be loaded across scenes
         manager = (StoryManager)FindAnyObjectByType(typeof(StoryManager));
     }
 
     public void Trigger()
     {
-        manager.StoryEvent(info);
+        if (manager != null) {
+            manager.StoryEvent(info);
+        }
     }
 
     public void Trigger(StoryEventInfo info)
