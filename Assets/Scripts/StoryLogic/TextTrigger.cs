6e4881ef71f552a34eb34b21f4e5b620ab743b71
Coolaconsole <93737386+Coolaconsole@users.noreply.github.com>
2026-01-05 22:34:55 +0000

    Event to complete certain text boxes defined

---
+++ Assets/Scripts/StoryLogic/TextTrigger.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class TextTrigger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Call the method in TextManager to complete the text prompt event
+            TextManager.Instance.CompleteCurrentPrompt();
+        }
+    }
+}
